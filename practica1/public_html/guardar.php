7605abce38b7e767616305e775e23354ef0f3145
Mudvayne084 <c16_davidk4@hotmail.com>
2014-06-27 16:04:22 -0500

    Mundial

---
+++ practica1/public_html/guardar.php
@@ -0,0 +1,21 @@
+<?php
+  echo "Hola";
+  
+  echo "Goles Equipo 1 Local:" .$_POST['equipo1_local'];
+  echo "<br />";
+  echo "Goles Equipo 2 Visitante:" .$_POST['equipo2_visitante'];
+  
+  echo "<br />";
+  echo "Goles Equipo 3 Local:" .$_POST['equipo3_local'];
+  echo "<br />";
+  echo "Goles Equipo 4 Visitante:" .$_POST['equipo4_visitante'];
+  
+  echo "<br />";
+  echo "Equipo Seleccionado:" .$_POST['SelectEquipos_local'];
+  echo "<br />";
+  echo "Goles Equipo 5 Local:" .$_POST['equipo5_local'];
+  echo "<br />";
+  echo "Equipo Seleccionado:" .$_POST['SelectEquipos_visitante'];
+  echo "<br />";
+  echo "Goles Equipo 6 Visitante:" .$_POST['equipo6_visitante'];
+?>
